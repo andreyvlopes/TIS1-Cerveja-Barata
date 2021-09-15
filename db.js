9e20d5b0d4c55ab482bffa321a4294f892e8d1db
andrey <andrey.lopes@teknisa.com>
2021-09-15 16:32:17 -0300

    first commit

---
+++ db.js
@@ -0,0 +1,234 @@
+let dbbares = {
+  data: [
+    {
+      id: 1,
+      estabelecimento: "Nimbos Bar",
+      horario_de_funcionamento: "18:00-00:00",
+      image: "https://lh5.googleusercontent.com/p/AF1QipM1RAxq8L5EYum4hrhgHr7x6MTD4FWXR7OONE0i=w408-h306-k-no",
+      mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.8134283961444!2d-43.93760938569819!3d-19.932265486602546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x8b6b021b211e486!2sNimbos%20Bar!5e0!3m2!1spt-BR!2sbr!4v1575056448424!5m2!1spt-BR!2sbr" width="600" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>' ,
+      produtos: [
+        {
+          categoria: "cerveja",
+          recipiente: "garrafa",
+          marca: "Skol",
+          preço: 10.0
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "garrafa",
+          marca: "Heineken",
+          preço: 12.75
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "garrafa",
+          marca: "Brahma",
+          preço: 10.0
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "long neck",
+          marca: "Skol",
+          preço: 6.0
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "long neck",
+          marca: "Brahma",
+          preço: 7.0
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "long neck",
+          marca: "Heineken",
+          preço: 8.0
+        },
+        {
+          categoria: "chop",
+          recipiente: "chop",
+          marca: "Skol",
+          preço: 6.0
+        },
+        {
+          categoria: "chop",
+          recipiente: "chop",
+          marca: "Brahma",
+          preço: 7.25
+        }
+      ]
+    },
+    {
+      id: 2,
+      estabelecimento: "Peixe Boi Restaurante, Bar e petisqueira",
+      horario_de_funcionamento: "11:00-16:00",
+      image: "https://lh5.googleusercontent.com/p/AF1QipNwZZqXVnsCKC0ISFDsBf8hh-E2v5jToM7K7UJP=w426-h240-k-no",
+      mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2045.1296801573762!2d-43.93593822297296!3d-19.93349272505726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xedc56b2afdffb034!2sPeixe%20Boi%20Restaurante%2C%20Bar%20e%20petisqueira!5e0!3m2!1spt-BR!2sbr!4v1575056623560!5m2!1spt-BR!2sbr" width="600" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>' ,
+      produtos: [
+        {
+          categoria: "cerveja",
+          recipiente: "garrafa",
+          marca: "Skol",
+          preço: 10.50
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "garrafa",
+          marca: "Heineken",
+          preço: 12.0
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "garrafa",
+          marca: "Brahma",
+          preço: 10.70
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "long neck",
+          marca: "Skol",
+          preço: 6.20
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "long neck",
+          marca: "Brahma",
+          preço: 7.0
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "long neck",
+          marca: "Heineken",
+          preço: 8.0
+        },
+        {
+          categoria: "chop",
+          recipiente: "chop",
+          marca: "Skol",
+          preço: 7.30
+        },
+        {
+          categoria: "chop",
+          recipiente: "chop",
+          marca: "Brahma",
+          preço: 7.20
+        }
+      ]
+    },
+    {
+      id: 3,
+      estabelecimento: "Agnaldo's Bar",
+      endereço: "Av. Brasil, 1900-2000 - Santa Efigênia, Belo Horizonte - MG, 30140-100",
+      horario_de_funcionamento: "00:00-00:00",
+      image: "https://geo3.ggpht.com/cbk?panoid=Uj6NSOPnRS3za_cWwlftYg&output=thumbnail&cb_client=search.gws-prod.gps&thumb=2&w=408&h=240&yaw=334.80966&pitch=0&thumbfov=100",
+      mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3644.0079612710497!2d-43.9361410743831!3d-19.933447637573952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x23d2928cc57a7ba1!2sAgnaldo&#39;s%20Bar!5e0!3m2!1spt-BR!2sbr!4v1575050872849!5m2!1spt-BR!2sbr" width="600" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>' ,
+      produtos: [
+        {
+          categoria: "cerveja",
+          recipiente: "garrafa",
+          marca: "Skol",
+          preço: 9.50
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "garrafa",
+          marca: "Heineken",
+          preço: 13.0
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "garrafa",
+          marca: "Brahma",
+          preço: 9.90
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "long neck",
+          marca: "Skol",
+          preço: 6.69
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "long neck",
+          marca: "Brahma",
+          preço: 6.20
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "long neck",
+          marca: "Heineken",
+          preço: 7.0
+        },
+        {
+          categoria: "chop",
+          recipiente: "chop",
+          marca: "Skol",
+          preço: 7.0
+        },
+        {
+          categoria: "chop",
+          recipiente: "chop",
+          marca: "Brahma",
+          preço: 7.50
+        }
+      ]
+    },
+     {
+      id: 4,
+      estabelecimento: "EMME Lounge e Food",
+      endereço: "Av. Brasil, 1900-2000 - Santa Efigênia, Belo Horizonte - MG, 30140-100",
+      horario_de_funcionamento: "00:00-00:00",
+      image: "http://www.saboreie.com/portal/wp-content/uploads/2019/01/GINTERIA1-e1532474123692.jpg",
+      mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.7573449320344!2d-43.93591329367499!3d-19.934627819926877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xbb2cbf4ff04434e8!2sEMME%20Lounge%20e%20Food!5e0!3m2!1spt-BR!2sbr!4v1575059278865!5m2!1spt-BR!2sbr" width="600" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>' ,
+      produtos: [
+        {
+          categoria: "cerveja",
+          recipiente: "garrafa",
+          marca: "Skol",
+          preço: 13.50
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "garrafa",
+          marca: "Heineken",
+          preço: 15.0
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "garrafa",
+          marca: "Brahma",
+          preço: 12.90
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "long neck",
+          marca: "Skol",
+          preço: 8.0
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "long neck",
+          marca: "Brahma",
+          preço: 8.60
+        },
+        {
+          categoria: "cerveja",
+          recipiente: "long neck",
+          marca: "Heineken",
+          preço: 9.20
+        },
+        {
+          categoria: "chop",
+          recipiente: "chop",
+          marca: "Skol",
+          preço: 8.55
+        },
+        {
+          categoria: "chop",
+          recipiente: "chop",
+          marca: "Brahma",
+          preço: 8.30
+        }
+      ]
+    }
+  ]
+};
